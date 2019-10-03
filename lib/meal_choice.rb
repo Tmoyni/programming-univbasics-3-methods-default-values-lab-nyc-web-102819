50b4a18a2462e3da9498d0a041393f6dd113a085
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-03 17:53:30 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,7 +8,7 @@
 #greeting_programmer("Maria")
 # > Hello, Maria. We heard you are a great computer programmer.
 
-def meal_choice(veg1, veg2, protein = "meat")
+def meal_choice(veg1, veg2, protein ="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
