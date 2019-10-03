0b0a31046603bc1c88f76d492c764a314cb14848
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-03 18:00:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,13 +1,3 @@
-#def greeting_programmer(name, language = "computer")
- # puts "Hello, #{name}. We heard you are a great #{language} programmer."
-#end
- 
-#greeting_programmer("Steven", "Ruby")
-# > Hello, Steven. We heard you are a great Ruby programmer.
- 
-#greeting_programmer("Maria")
-# > Hello, Maria. We heard you are a great computer programmer.
-
 def meal_choice(veg1, veg2, protein ="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
